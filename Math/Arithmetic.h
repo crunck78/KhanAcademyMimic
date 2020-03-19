14275c0eaf21e50e909f29291ec0c735b98ebf0c
Neacsu Mihai-Andrei <scoo_andrew@yahoo.com>
2020-03-19 20:32:58 +0100

    Add Code to Files

--- Math/Arithmetic.h
+++ Math/Arithmetic.h
@@ -1,1 +1,62 @@
 #pragma once
+
+float sumOf(float a, float b)
+{
+	return (a + b);
+}
+
+float diffOf(float a, float b)
+{
+	return (a - b);
+}
+
+float multOf(float a, float b)
+{
+	return (a * b);
+}
+
+float divOf(float a, float b)
+{
+	//TODO: Guard against 0 division
+	return (a / b);
+}
+
+float power(float base, int expon)
+{
+	if (expon == 1)
+		return expon;
+	int temp = base;
+	for (int i = 1; i < expon; i++)
+	{
+		temp = multOf(temp, base);
+	}
+	return temp;
+}
+
+bool isPossitive(float a)
+{
+	return (a > 0.0f);
+}
+
+bool isNegative(float a)
+{
+	return (a < 0.0f);
+}
+
+bool isOpposite(float a, float b)
+{
+	return sumOf(a, b) == 0.0f;
+}
+
+float opposite(float a)
+{
+	return diffOf(a, multOf(2.0f, a));
+}
+
+float absoluteValue(float a)
+{
+	if (isNegative(a))
+		return opposite(a);
+
+	return a;
+}
--- Math/BasicGeometry.h
+++ Math/BasicGeometry.h
@@ -1,1 +1,47 @@
 #pragma once
+
+#include "Constants.h"
+
+float areaRect(float length, float heigth)
+{
+	return multOf(length, heigth);
+}
+
+float areaTriangle(float base, float heigth)
+{
+	return divOf(areaRect(base, heigth), 2.0f);
+}
+
+float circumOfDiam(float diameter)
+{
+	return (diameter * PHI);
+}
+
+float diamOfCircum(float circumference)
+{
+	return (circumference / PHI);
+}
+
+float areaCircle(float radius)
+{
+	return (PHI * power(radius, 2));
+}
+
+float volumOfRectPrism(float length, float width, float heigth)
+{
+	return(length * width * heigth);
+}
+
+float areaOfRectPrism(float length1, float length2, float length3)
+{
+	float sidesArea = areaRect(length1, length2) * 2.0f;
+	float topBottomArea = areaRect(length1, length3) * 2.0f;
+	float frontBackArea = areaRect(length2, length3) * 2.0f;
+
+	return (sidesArea + topBottomArea + frontBackArea);
+}
+
+float volumOfTriangularPrism(float base, float heigth, float length)
+{
+	return (base * heigth * length) / 2.0f;
+}
