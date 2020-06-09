331ae10fdefaa724ec61653cd53112001e5f3584
Mihai-Andrei Neacsu <scoo_andrew@yahoo.com>
2020-06-09 16:04:46 +0200

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <iostream>
+#include "Menu.h"
+
+int main()
+{
+	
+	initMenuTree();
+	Menu *actual = &K_main;
+	while (actual != &K_exit)
+	{
+		actual->displayOptions();
+		actual = actual->selectOption();
+		//system("cls"); moved in class menu, selectOption methode
+	}
+}
